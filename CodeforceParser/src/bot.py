c7a7e5ed2aac298bd4e4ff45b6c1087374ef832f
Nikolay Chernikov <nikolay.chernikov.dev@gmail.com>
2023-03-09 16:30:26 +0300

    refactor: delete unused function

--- CodeforceParser/src/bot.py
+++ CodeforceParser/src/bot.py
@@ -1,28 +1,22 @@
 import telebot
+import configparser
 from telebot import types
-from main import DataBase
+from database import DataBase
 
-bot = telebot.TeleBot('6042483401:AAGAPECY9w52vEZBH83Un5JI6JFUizwz7fE')
+config = configparser.ConfigParser()
+config.read("../deployment/config/config.conf")
+bot = telebot.TeleBot(str(config["BOT"]["token"]))
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "👋 Привет! Я твой бот-помошник!", reply_markup=markup)
+    btn1 = types.KeyboardButton('Выбрать сложность и тему')
+    btn2 = types.KeyboardButton('Выбрать по номеру задачи')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, 'Выбери что хочешь', reply_markup=markup)
 
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == '👋 Поздороваться':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
-        btn1 = types.KeyboardButton('Выбрать сложность и тему')
-        btn2 = types.KeyboardButton('Выбрать по номеру задачи')
-        markup.add(btn1, btn2)
-        bot.send_message(message.from_user.id, 'Выбери что хочешь', reply_markup=markup)  # ответ бота
-
-    elif message.text == 'Выбрать сложность и тему':
+    if message.text == 'Выбрать сложность и тему':
         bot.send_message(message.chat.id, "Введите сложность и тему через пробел: ")
         bot.register_next_step_handler(message, find_by_difficulty_theme)
 
@@ -34,8 +28,7 @@ def get_text_messages(message):
 def find_by_id(message):
     db = DataBase()
     a = str(message.text)
-    db.cur.execute('''SELECT * FROM codeforseparser WHERE id = %s;''', (a,))
-    value = db.cur.fetchall()
+    value = db.find_by_id(a)
     returned_string = ""
     for i in range(len(value)):
         returned_string += str(value[i])
@@ -50,8 +43,7 @@ def find_by_difficulty_theme(message):
     a = a.split(" ")
     dif = a[0]
     theme = a[1]
-    db.cur.execute('''SELECT * FROM codeforseparser WHERE level = %s AND themes = %s LIMIT 10;''', (dif,theme))
-    value = db.cur.fetchall()
+    value = db.find_by_difficulty_theme(dif, theme)
     value = ' '.join(str(el) for el in value)
 
     bot.send_message(message.from_user.id,
@@ -59,4 +51,4 @@ def find_by_difficulty_theme(message):
                      parse_mode='Markdown')
 
 
-bot.polling(none_stop=True, interval=0)  # обязательная для работы бота часть
+bot.polling(none_stop=True, interval=0)
