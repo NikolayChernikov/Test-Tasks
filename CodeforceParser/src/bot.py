14275eec400649d4b080cdd958dda990ad364a6d
Nikolay Chernikov <nikolay.chernikov.dev@gmail.com>
2023-03-09 16:44:19 +0300

    feat: change output format

--- CodeforceParser/src/bot.py
+++ CodeforceParser/src/bot.py
@@ -16,8 +16,11 @@ def start(message):
     markup.add(btn1, btn2)
     bot.send_message(message.from_user.id, 'Выбери что хочешь', reply_markup=markup)
 
+
+@bot.message_handler(content_types=["text"])
+def get_text_messages(message):
     if message.text == 'Выбрать сложность и тему':
-        bot.send_message(message.chat.id, "Введите сложность и тему через пробел: ")
+        bot.send_message(message.chat.id, "Введите сложность: ")
         bot.register_next_step_handler(message, find_by_difficulty_theme)
 
     elif message.text == 'Выбрать по номеру задачи':
@@ -38,14 +41,21 @@ def find_by_id(message):
 
 
 def find_by_difficulty_theme(message):
-    db = DataBase()
-    a = str(message.text)
-    a = a.split(" ")
-    dif = a[0]
-    theme = a[1]
-    value = db.find_by_difficulty_theme(dif, theme)
-    value = ' '.join(str(el) for el in value)
+    data = []
+    diff = message.text
+    data.append(diff)
+    bot.send_message(message.chat.id, "Введите тему")
+    bot.register_next_step_handler(message, get_url, data)
 
+
+def get_url(message, data):
+    data.append(message.text)
+    db = DataBase()
+    value = db.find_by_difficulty_theme(data[0], data[1])
+    value = ' '.join(str(el) for el in value)
+    value = value.split(") (")
+    value = '\n'.join(value)
+    print(value)
     bot.send_message(message.from_user.id,
                      value,
                      parse_mode='Markdown')
